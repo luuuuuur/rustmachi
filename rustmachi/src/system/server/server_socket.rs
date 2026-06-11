b9299d9770756be3ab86809f7dd632f701fe0ad3
luuuuuur <plazasjolifenzio@gmail.com>
2026-06-11 03:21:14 -0400

    fix server bug

--- rustmachi/src/system/server/server_socket.rs
+++ rustmachi/src/system/server/server_socket.rs
@@ -1,13 +1,13 @@
 //creates the Tcp Socket for the TCP stream
 //tcp sockets creates from the json file
 //so we need the data from the setup_server
-use std::net::{SocketAddrV4};
+use std::net::{Ipv4Addr, SocketAddrV4};
 use crate::system::server::server_tun::Tunnel;
 
 
 //get json data -> creates socket
 pub fn create(tunnel: Tunnel) -> SocketAddrV4{
-    let socket: SocketAddrV4 = SocketAddrV4::new(tunnel.setup.get_ip(), tunnel.setup.get_port());
+    let socket: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), tunnel.setup.get_port());
     socket
 }
 
